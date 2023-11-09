ac075b57ca669929de1d5ce136df19f29790b45c
ekajdfk <yoosy320@gmail.com>
2023-11-09 15:49:49 +0900

    5

--- main.c
+++ main.c
@@ -4,9 +4,23 @@
 
 int main(int argc, char *argv[])
 {
-  char str[30] = "happy C programming";
+  FILE* fp;
+  char str[100];
+  int i;
   
-  printf("문자열\"%s\"의 길이: %i\n", str, strlen(str));
+  //1. open file
+  fp = fopen("sample.txt", "w");
+  
+  //2. write file
+  for(i = 0; i < 3; i++)
+  {
+        printf("input a word: ");
+        scanf("%s", str);
+        fprintf(fp, "%s\n", str);
+  }
+  
+  //3. close file
+  fclose(fp);
   
   system("PAUSE");	
   return 0;
